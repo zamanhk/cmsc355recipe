503d21671b7ea9987874227f730ca7f0605afdc2
Kevin Ly <kevinly277@gmail.com>
2020-09-30 22:00:37 -0400

    Added Button to XML file

--- RecipeApp/app/src/main/java/com/example/android/recipeapp/filterSearch.java
+++ RecipeApp/app/src/main/java/com/example/android/recipeapp/filterSearch.java
@@ -8,7 +8,8 @@ public class filterSearch extends AppCompatActivity
 {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter_search);
     }
