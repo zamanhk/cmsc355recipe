de794a989170872e7b4bcf48710bf30d2224c551
Kevin Ly <kevinly277@gmail.com>
2020-09-30 21:43:08 -0400

    Testing Commit

--- RecipeApp/app/src/main/java/com/example/android/recipeapp/filterSearch.java
+++ RecipeApp/app/src/main/java/com/example/android/recipeapp/filterSearch.java
@@ -4,7 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class filterSearch extends AppCompatActivity {
+public class filterSearch extends AppCompatActivity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
